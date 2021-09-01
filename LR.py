ee7be0ef97b43ae428ada7f6ae34959157ea5375
Liu373 <87485446+Liu373@users.noreply.github.com>
2021-08-31 23:10:32 -0400

    Update LR.py

--- LR.py
+++ LR.py
@@ -87,6 +87,127 @@ def Data_Extraction(Path, Input_Scope, Column_Name):
 
         
         data_Scope_Not_NA_Gp.to_csv(Output_File)
+        
+        
+        print('\n')
+        print('{0} is done'.format(f))
+        
+        
+      else:
+        print('\n')
+        print('{0} still has Null after Data Cleaning'.format(f))
+        
+        
+        
+        
+        
+def LR_Training(Path, Train_File_Name):
+  
+  File_Path = Path + '\\' + Train_File_Name
+  
+  Input_File = pd.read_csv(File_Path, low_memory=False)
+  Input_File = Input_File.droop('Unnamed: 0', 1)
+  
+  Input_File_1 = Input_File[Input_File['CSCORE_B'] > 499]
+  Input_File_1['OLTV'] = Input_File_1['OLTV'].replace(0, Input_File_1['OLTV'].mean())
+  Input_File_1['NUM_BO'] = Input_File_1['NUM_BO'].replace(0, Input_File_1['NUM_BO'].mean())
+  Input_File_1['NUM_BO_Min'] = Input_File_1['NUM_BO'].map(lambda x: x if x>0.99999 else 1)
+  Input_File_1['DTI'] = Input_File_1['DTI'].replace(0, Input_File_1['DTI'].mean())
+  
+  X = np.asarray(Input_File_1[["OLTV", "NUM_BO_Min", "CSCORE_B", "CHANNEL_B", "CHANNEL_C", "CHANNEL_R", "OCC_STAT_I", "OCC_STAT_P", "OCC_STAT_S"]])
+  
+  Y = np.asarray(Input_File_1['Default_5_FM'])
+  
+  scaler = MinMaxScaler()
+  X_normal = scaler.fit_transform(X)
+  
+  LR = LogisticRegression(C=0.1, solver='liblinear', penalty='l2').fit(X_normal, Y)
+  
+  print('Intercept is ', LR.intercept_)
+  print('Coefficients are ', LR.coef_)
+  
+  return LR, X_normal, Y
+
+
+
+def LR_Testing(Path, Test_File_Name, LR):
+  
+  # OrigD        = [0.003,    0.006,    0.009,   0.012,    0.015,  0.018,   ......]
+  # RecessionD   = [0.009099, 0.032466, 0.05148, 0.067732, 0.0837, 0.09655, ......]
+  
+  # D_interp = scipy.interpolate.interpld(OrigD, RecessionD)
+  
+  Period = Test_File_Name[:13][7:]
+  File_Path = Path + '\\' + Test_File_Name
+  
+  Input_File = pd.read_csv(File_Path, low_memory=False)
+  
+  Input_File['Orig_Date_Str'] = Input_File['ORIG_DATE'].astype(str)
+  Input_File['Orig_Date'] = Input_File['Orig_Date_Str'].map(lambda x: datetime(year=int(float(x[-6:])), month=int(x[:2]), day=1) if len(x)==8 else
+                                                           (datetime(year=int(float(x[-6:])), month=int(x[:1]), day=1) if len(x)==7 else 0))
+  
+  
+  Input_File['FiveYear_Date'] = Input_File['Orig_Date'] + timedelta(days=1825)
+                                                            
+  Input_File['Foreclosure_Str'] = Input_File['FORECLOSURE_DATE'].astype(str)
+  Input_File['Foreclosure_Date'] = Input_File['Foreclosure_Str'].map(lambda x: datetime(year=int(float(x[-6:])), month=int(x[:2]), day=1) if len(x)==8 else
+                                                                    (datetime(year=int(float(x[-6:])), month=int(x[:1]), day=1) if len(x)==7 else
+                                                                    datetime(year=1995, month=1, day=1)))
+  
+  Input_File['Default_5'] = np.where((Input_File['FORECLOSURE_DATE']!=0) & (Input_File['Foreclosure_Date']<Input_File['FiveYear_Date']), 1, 0)
+  
+  Input_File['Default_5_FM'] = np.where((Input_File['Default_5']==1) & ((Input_File['Zero_Bal_Code']==9) | (Input_File['Zero_Bal_Code']==3)), 1, 0)
+  
+  Input_File = Input_File.drop('Orig_Date_Str', 1)
+  Input_File = Input_File.drop('Foreclosure_Str', 1)
+  
+  Input_File = Input_File[Input_File['CSCORE_B']>0.999999]
+  Input_File = Input_File[Input_File['OLTV']>0.9999999]
+  Input_File = Input_File[Input_File['NUM_BO']>0.999999]
+  Input_File = Input_File[Input_File['DTI']>0.999999]
+  
+  X = np.asarray(Input_File[["OLTV", "NUM_BO_Min", "CSCORE_B", "CHANNEL_B", "CHANNEL_C", "CHANNEL_R", "OCC_STAT_I", "OCC_STAT_P", "OCC_STAT_S"]])
+  
+  Old_LoanID = Input_File['LOAN_ID'].tolist()
+  
+  scaler = MinMaxScaler()
+  X_normal = scaler.fit_transform(X)
+  Yhat = LR.predict(X_normal)
+  Yhat_Prob = LR.predict_proba(X_normal)[:, 1]
+  
+  Yhat_Prob_Df = pd.DataFrame(Yhat_Prob, index=Old_LoanID, columns=['Yhat_Prob'])
+  Yhat_Prob_Df.index.names = ['LOAN_ID']
+  
+  Yhat_Df = pd.DataFrame(Yhat, index=Old_LoanID, columns=['Yhat'])
+  Yhat_Df.index.names = ['LOAN_ID']
+  
+  Input_File.set_index('LOAN_ID', inplace=True)
+  
+  Input_File = Input_File.merge(Yhat_Prob_Df['Yhat_Prob'], on='LOAN_ID')
+  Input_File = Input_File.merge(Yhat_Df['Yhat'], on='LOAN_ID')
+  
+  Input_File['Annualized_Yhat_Prob'] = Input_File['Yhat_Prob'].map(lambda x: 1-((1-x)**(1/5)))
+  Input_File['Annualized_Stressed_PD'] = norm.cdf((norm.ppf(Input_File['Annualized_Yhat_Prob']) + np.sqrt(0.15) * norm.ppf(0.999))/np.sqrt(1-0.15))
+  Input_File['Stressed_PD'] = Input_File['Annualized_Stressed_PD'].map(lambda x: ((1+x)**5) -1 )
+  
+  pd_bins = np.linspace(0, 0.06, num=21)
+  
+  group_pd_7 = (Input_File[['Yhat_Prob', 'Default_5_FM']].groupby([pd.cut(Input_File['Yhat_Prob'], pd_bins)]). agg({'Yhat_Prob': ['count', 'mean'],
+                                                                                                                    'Default_5_FM': 'sum'}))
+  
+  
+  group_pd_8 = (Input_File[['Stressed_PD', 'Default_5_FM']].groupby([pd.cut(Input_File['Yhat_Prob'], pd_bins)]). agg({'Stressed_PD': ['count', 'mean'],
+                                                                                                                      'Default_5_FM': 'sum'}))
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 
 
 
